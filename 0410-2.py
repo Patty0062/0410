5a178bdb1abf832ac01ce96a320330700e8341b7
Patty0062 <144547485+Patty0062@users.noreply.github.com>
2026-04-11 01:44:28 +0800

    Update 0410-2.py

--- 0410-2.py
+++ 0410-2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import random
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
